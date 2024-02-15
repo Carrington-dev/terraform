e86cdaa4b320d608b2db2ef14b9b973246e9f688
Carrington-dev <crn96m@gmail.com>
2024-02-15 10:27:38 +0200

    Added AWS and AZURE prodivers config

---
+++ README.md
@@ -0,0 +1,14 @@
+# Terraform Tutorials
+
+## To print all logs
+
+```bash
+export TF_LOG=TRACE
+```
+
+## Basic Commands
+
+```bash
+terraform init 
+# same as git init
+```
---
+++ prodivers/main.tf
@@ -0,0 +1,15 @@
+# # Providers Exmaples
+
+# Configue AWS Providers
+provider "aws" {
+    region = "us-east-1"
+    version = "5.36.0"
+}
+
+# Configue AZURE Providers
+provider "azurerm" {
+    version = "3.91.0"
+    feature {
+
+    }
+}
