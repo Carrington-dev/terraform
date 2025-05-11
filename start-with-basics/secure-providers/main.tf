1f2c52bb218044a6da2cf05cb5bb54e11f2b7c4a
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-11 14:24:54 +0200

    Secrets Added

--- start-with-basics/secure-providers/main.tf
+++ start-with-basics/secure-providers/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
     version = "3.53.0"
     access_key = var.AWS_ACCESS_KEY
     secret_key = var.AWS_SECRET_KEY
-    region     = var.AWS_REGION
+    region     =  "us-east-1" # var.AWS_REGION
 }
 
 # Configure the Microsoft Azure Provider
