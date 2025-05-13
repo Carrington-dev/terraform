4a3dffecec0eda2d9f220c4df51bdf33ccf9a5d3
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-13 21:04:11 +0200

    secure

--- start-with-basics/secure-providers/main.tf
+++ start-with-basics/secure-providers/main.tf
@@ -3,8 +3,8 @@
 # Configure the AWS Provider
 provider "aws" {
     version = "3.53.0"
-    access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SECRET_KEY
+    #access_key = var.AWS_ACCESS_KEY
+    #secret_key = var.AWS_SECRET_KEY
     region     =  "us-east-1" # var.AWS_REGION
 }
 
