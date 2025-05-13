4f94d86beac2187ef9ff2bcc8094e9848097884a
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-13 21:05:10 +0200

    Update main.tf

--- start-with-basics/secure-instance-creation/main.tf
+++ start-with-basics/secure-instance-creation/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "MyT2MicroInstanceAWS" {
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (for us-east-1)
   instance_type = "t2.micro"
 
   tags = {
-    Name = "MyT2MicroInstance"
+    Name = "MyT2MicroInstanceAWS"
   }
 }
