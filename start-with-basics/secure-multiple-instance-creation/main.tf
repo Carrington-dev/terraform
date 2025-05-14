2eef9c830846c679578bdf4087de35e5289b8b26
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-14 10:01:04 +0200

    Create main.tf

---
+++ start-with-basics/secure-multiple-instance-creation/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+
+
+resource "aws_instance" "MyT2MicroInstanceAWS" {
+  count = 4
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyT2MicroInstanceAWS-${count.index}"
+  }
+}
