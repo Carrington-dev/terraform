36526dba8c91ff0883ee9a12669ad21465c7f319
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-14 12:26:30 +0200

    AWS_SECURITY_GROUP

--- start-with-basics/variables-list-and-maps/main.tf
+++ start-with-basics/variables-list-and-maps/main.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "MyT2MicroInstanceAWS" {
   count         = 4
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
+  security_groups = "${var.AWS_SECURITY_GROUP}"
 
   tags = {
     Name = "MyT2MicroInstanceAWS-${count.index}"
