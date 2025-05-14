f474c2ebc9d11ef5e4280b38a86bff37465744e3
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-14 11:27:33 +0200

    Update main.tf

--- start-with-basics/secure-providers/main.tf
+++ start-with-basics/secure-providers/main.tf
@@ -13,3 +13,15 @@ provider "azurerm" {
     version = "2.72.0"
     features {}
 }
+
+
+
+resource "aws_instance" "MyT2MicroInstanceAWS" {
+  count         = 4
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyT2MicroInstanceAWS-${count.index}"
+  }
+}
