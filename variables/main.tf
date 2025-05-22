530d0c5b3146b8817277180a2152f81e2feb53c3
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-22 10:27:32 +0200

    Create main.tf

---
+++ variables/main.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    default = "Mulalo"
+    type = string
+    description = "Name of the Repository Author"
+}
+
+variable "surname" {
+    default = "Muleya"
+    type = string
+    description = "surname of the Repository Author"
+}
